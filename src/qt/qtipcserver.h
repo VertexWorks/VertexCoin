755e0c3236ef33828e3f734f816d2eee60d4e690
CryptoVertex <CryptoVertex@users.noreply.github.com>
2015-06-23 22:07:49 +0400

    Vertex

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Vertex-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "VertexURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
